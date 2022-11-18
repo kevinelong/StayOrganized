84404392b2d489378bac4f9c6c0233590efddfff
kevinelong <kevinelong@gmail.com>
2022-11-18 09:47:36 -0800

    filter users

---
+++ demo.js
@@ -0,0 +1,20 @@
+
+// create a variable X and put the rult of an expression (1+2) into the variable.
+const x = 1 + 2;
+console.log( x )
+
+let y = 123;
+y = 456;
+console.log( y );
+
+//////
+
+const isMatch = false;
+
+if(isMatch){
+    console.log("found")
+}else{
+    console.log("nope")
+}
+
+console.log( isMatch ? "found" : "nope"); //ternary operator (three-way)
